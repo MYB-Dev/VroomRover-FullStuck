fa4c045e49d3717bcb24dfc4a0658a62456c50be
MYB Dev <mybdev20@gmail.com>
2021-07-07 09:11:18 +0100

    fix Json

--- config/socket.js
+++ config/socket.js
@@ -6,9 +6,8 @@ const socketIO = (server, wss, option) => {
     console.log(`made socket connection with id: ${socket.id}`)
 
     socket.on('detectObj', (raw) => {
-      const data = JSON.stringify(raw)
-      console.log(data)
-      // socket.broadcast.emit('sensor', data)
+      const data = JSON.parse(raw)
+      socket.broadcast.emit('sensor', data)
     })
     socket.on('emit_command', (raw) => {
       socket.broadcast.emit('command_rover', raw)
